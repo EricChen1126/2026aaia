011cafc2d22fdd480007a04672ed1b164dede4df
EricChen1126 <167710240+EricChen1126@users.noreply.github.com>
2026-03-20 16:37:48 +0800

    Fix formatting issues in week 04-3.cpp
    新增commit內容。

--- github用/week 04-3.cpp
+++ github用/week 04-3.cpp
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 int main () {
+    
     int ans, now = 0;
+    
     for (int i = 0; i < 10; i++) {
         scanf("%d", &now);
         if (now % 3 == 0) ans++;
     }
     printf("%d\n", ans);
+    
 }
