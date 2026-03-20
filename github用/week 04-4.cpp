c69ce30178a057adb1f0bae3bf25a93ddb2c7e7d
EricChen1126 <167710240+EricChen1126@users.noreply.github.com>
2026-03-20 16:41:04 +0800

    week04
    week04

--- github用/week 04-4.cpp
+++ github用/week 04-4.cpp
@@ -3,11 +3,15 @@
 #include <stdio.h>
 
 int main () {
+    
     int a, b = 0;
+    
     scanf("%d%d", &a, &b);
+    
     for (int i = a; i <= b; i++) {
         if (i % 5 == 0) printf("%d\n", i);
     }
+    
     for (int i = b; i <= a; i++) {
         if (i % 5 == 0) printf("%d\n", i);
     }
